069bc0c1608489d7f889a1a39f09abd458d9df9f
olarik surinta <35139872+mrolarik@users.noreply.github.com>
2025-03-26 22:31:19 +0700

    Update index.py

--- index.py
+++ index.py
@@ -15,6 +15,6 @@ if uploaded_file is not None:
     img = image.resize((224, 224))
     img_array = np.array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
+    
 
     
